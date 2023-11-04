82b51456beb57832e711f4a687ae051c61c732ee
Andrey Khalepov <w54661c@gmail.com>
2023-11-04 13:38:55 +1000

    overload done

---
+++ overload/overload.ts
@@ -0,0 +1,20 @@
+class User {
+    skills: string[];
+
+    addSkill(skill: string): void;
+    addSkill(skills: string[]): void;
+
+    addSkill(skillOrSkills: string | string[]): void {
+        if (typeof skillOrSkills === 'string') {
+            this.skills.push(skillOrSkills);
+        } else {
+            this.skills.push(...skillOrSkills);
+        }
+    }
+}
+
+const user = new User();
+
+user.addSkill('dev');
+
+user.addSkill(['devops', 'design']);
