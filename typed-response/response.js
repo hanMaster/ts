a35191aac638ef0de73c0111129715c8c35fd9f5
Andrey Khalepov <w54661c@gmail.com>
2023-11-04 12:49:59 +1000

    homework1 done

---
+++ typed-response/response.js
@@ -0,0 +1,6 @@
+"use strict";
+var PaymentStatus;
+(function (PaymentStatus) {
+    PaymentStatus["SUCCESS"] = "success";
+    PaymentStatus["FAILED"] = "failed";
+})(PaymentStatus || (PaymentStatus = {}));
