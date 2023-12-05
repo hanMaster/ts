e03911cbb15ee7cd093723b2756a1dc9086e6761
Andrey Khalepov <w54661c@gmail.com>
2023-12-05 21:10:56 +1000

    template method implemented

--- command-executor/src/app.ts
+++ command-executor/src/app.ts
@@ -1,6 +1,11 @@
+import { PromptService } from "./core/prompt/prompt.service";
+import { ConsoleLogger } from "./out/console-logger/console-logger";
+
 class App {
-    run() {
-        console.log('Done');
+    async run() {
+        const res = await PromptService.input<number>('Введите число', 'number');
+        const sHandler = ConsoleLogger.getLogger();
+        sHandler.log(res);
     }
 }
 
